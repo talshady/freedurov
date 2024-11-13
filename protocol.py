67aabd47e68308e06fea83eea69209c797a903c6
talshady <68434728+talshady@users.noreply.github.com>
2024-11-13 08:01:25 +0000

    test

---
+++ protocol.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 64
+FORMAT = 'utf-8'
+
+
+def send( s, msg):
+    msglen = str(len(msg)).encode(FORMAT)
+    msglen += b' ' * (HEADER - len(msglen))
+    s.send(msglen)
+    s.send(msg.encode(FORMAT))
+
+
+
+
+
+def recv(s):
+    msglen = s.recv(HEADER).decode(FORMAT)
+    if msglen:
+        msglen = int(msglen)
+        msg = s.recv(msglen).decode(FORMAT)
+        return msg
+    return "failed"
