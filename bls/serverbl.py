d3189ad2df4e063659e55312f6745c49d3ea02b8
talshady <68434728+talshady@users.noreply.github.com>
2024-12-04 09:12:05 +0000

    idk added some stuff

---
+++ bls/serverbl.py
@@ -0,0 +1,10 @@
+def connect():
+    pass
+
+
+
+#registration
+def sendRegestery():
+    # Get login and password
+    #self.login = self.entry_login.get()
+    #self.password = self.entry_password.get()
